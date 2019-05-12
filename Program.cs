fbb0d387e7bee7e23f28e466ee65dd02052a4aac
Phoomparin Mano <phoomparin@gmail.com>
2019-05-13 04:01:32 +0700

    :tada:

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Hello {
+    public void hello() {
+        Console.WriteLine("Hello World!");
+    }
+}
+
+namespace HelloWorld {
+    static class Program {
+        private static void Main(string[] args) {
+            var hello = new Hello();
+
+            hello.hello();
+        }
+    }
+}
